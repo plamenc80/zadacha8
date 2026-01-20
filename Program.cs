3d3681ff8f5be2e7c0aad673c539b5b637c714c2
plamenc80 <chatalbashevp@gmail.com>
2026-01-20 15:42:13 +0200

    .

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿namespace HvurlqneNaZarZadacha8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const int throwsCount = 100000;
+
+            int[] results = RollDice(throwsCount);
+
+            Console.WriteLine($"Брой хвърляния: {throwsCount}\n");
+
+            for (int i = 0; i < results.Length; i++)
+            {
+                double probability = (double)results[i] / throwsCount;
+                Console.WriteLine(
+               $"Число {i + 1}: {results[i]} пъти -> Вероятност: {probability:P2}" );
+            }
+            Console.ReadLine();
+        }
+        static int[] RollDice(int throwsCount)
+        {
+            int[] counts = new int[6];
+            Random random = new Random();
+
+            for (int i = 0; i < throwsCount; i++)
+            {
+                int roll = random.Next(1, 7);
+                counts[roll - 1]++;
+            }
+            return counts;
+        }
+    }
+}
+
